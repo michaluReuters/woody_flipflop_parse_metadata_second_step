2556b9c71469aa31047359bae3a161071573f8a7
michalurbanski <michal.urbanski@thomsonreuters.com>
2023-02-02 14:38:16 +0100

    Initial commit

---
+++ domain/aws_actions/aws_actions.py
@@ -0,0 +1,33 @@
+import os
+
+import boto3 as boto3
+from botocore.exceptions import ClientError
+
+
+def find_valid_s3_prefix_dict(filename, prefix_dict):
+    prefix_name = prefix_dict['prefix-name']
+    file_format = prefix_dict['file-format']
+    configured_name = f"{prefix_name}{filename}{file_format}"
+    if file_in_s3_bucket(configured_name):
+        return prefix_dict
+    else:
+        pass
+
+
+def file_in_s3_bucket(file_name_sns) -> bool:
+    """
+    Checks if specified file exists in s3 bucket
+
+    :param:
+        file_name_sns: file that needs to be checked
+
+    :return:
+        bool: status
+    """
+    s3_bucket = boto3.resource("s3")
+    s3_bucket_name = os.environ.get("S3_BUCKET_NAME")
+    try:
+        s3_bucket.Object(s3_bucket_name, f"{file_name_sns}.xml").load()
+    except ClientError:
+        return False
+    return True
---
+++ lambda.py
@@ -0,0 +1,48 @@
+import boto3
+import json
+import os
+from aws_lambda_powertools import Logger
+from domain.aws_actions.aws_actions import find_valid_s3_prefix_dict
+
+logger = Logger()
+appconfig = boto3.client('appconfig')
+client = boto3.client('events')
+
+
+@logger.inject_lambda_context(log_event=True)
+def handler(event, context):
+    dict_event = event["message"]["detail"]
+    name = dict_event["file_name"]
+
+    configuration_prefixes = appconfig.get_hosted_configuration_version(
+        ApplicationId=os.environ.get('APP_CONFIG_APP_ID'),
+        ConfigurationProfileId=os.environ.get('APP_CONFIG_PREFIXES_ID'),
+        VersionNumber=int(os.environ.get("APP_CONFIG_PREFIXES_VERSION"))
+    )['Content'].read().decode('utf-8')
+
+    prefixes = json.loads(configuration_prefixes)
+
+    try:
+        valid_prefix_data = [find_valid_s3_prefix_dict(name, prefix) for prefix in prefixes]
+        valid_prefix = valid_prefix_data[0]['prefix-name']
+        valid_file_format = valid_prefix_data[0]['file-format']
+        configured_file_name = f"{valid_prefix}{name}{valid_file_format}"
+    except:
+        configured_file_name = "NO FILE WAS FOUND"
+
+    dict_event["metadata_path"] = configured_file_name
+
+    data_str = json.dumps(dict_event)
+
+    response = client.put_events(
+        Entries=[
+            {
+                'Source': "new-ppe-sonyhivemetadata-step2-json-complete",
+                'Resources': ["new-ppe-sh-sonyhive-metadata-import-step2-lambda"],
+                'DetailType': "metadata-step-complete",
+                'Detail': data_str
+            },
+        ]
+    )
+
+    logger.info(response)
